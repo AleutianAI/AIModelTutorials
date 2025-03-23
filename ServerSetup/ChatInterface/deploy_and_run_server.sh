b663c54bf3068046fa4eb516134f8ff1fb10d5a7
Jin <jinterlante@outlook.com>
2025-03-23 10:02:20 -0400

    fixed a dumb mistake

--- ServerSetup/ChatInterface/deploy_and_run_server.sh
+++ ServerSetup/ChatInterface/deploy_and_run_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VERSION=v1
+VERSION=v2
 
 # build the image
 podman build --runtime=runc -t go_chatbot_httpserver:$VERSION .
 
 # run the image
-podman run --runtime=runc go_chatbot_httpserver:$VERSION
+podman run --runtime=runc -p 12321:12321 go_chatbot_httpserver:$VERSION
